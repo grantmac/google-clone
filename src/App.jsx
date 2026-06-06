40c8efb8622d1cef3bcdf43e7d2e7379f47b352f
Ditty <ditty@users.noreply.github.com>
2026-06-06 12:58:57 +0000

    Rebrand Google to Superhands

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import bodyHtml from "./google-body.html?raw";
 
 export default function App() {
+  if (typeof document !== "undefined") {
+    document.title = "Superhands";
+  }
+
   return (
     <div
       id="google-clone-root"
